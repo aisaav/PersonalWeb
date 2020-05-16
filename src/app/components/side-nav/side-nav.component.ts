2946f12ee60b5e83c9f1353f5d4bb594562bec22
aisaav <aisaav@umich.edu>
2020-05-15 21:44:46 -0400

    progress so far, navbar fixed, need to fix font

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -7,9 +7,27 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SideNavComponent implements OnInit{
 
-  fillerNav = Array.of<string>('app-about', 'app-social', 'app-professional', 'app-hobbies-interests');
+  fillerNav = Array.of<string>('app-about', 'app-social', 'app-professional', 'app-hobbies-interests', 'app-contact');
 
   constructor() {
+  }
+  getDisplayName(nav: string){
+    if (nav === 'app-about'){
+      return 'About';
+    }
+    else if (nav === 'app-social'){
+      return 'Social';
+    }
+    else if (nav === 'app-professional'){
+      return 'Professional';
+    }
+    else if (nav === 'app-hobbies-interests'){
+      return 'Hobbies/Interests';
+    }
+    else if (nav === 'app-contact'){
+      return 'Contact';
+    }
+    return '';
   }
   ngOnInit(): void {
   }
