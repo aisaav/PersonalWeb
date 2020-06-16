b8b7b50f12c3539dd58c6bd035d85a610ba9c7cc
aisaav <aisaav@umich.edu>
2020-06-16 17:19:53 -0400

    updated main paged

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {ImageThemeService} from '../../Services/image-theme-service.service';
 import {Subscription} from 'rxjs';
 
@@ -8,8 +8,9 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit, OnDestroy {
-  breakpoint: number;
-  rowpoint: number;
+  width: number;
+  height: number;
+  prevInner: number;
   updateImg: boolean;
   subscription: Subscription;
   constructor(private imageThemer: ImageThemeService) {
@@ -20,13 +21,31 @@ export class MainPageComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.breakpoint = (window.innerWidth <= 500) ? 1 : 3;
-    this.rowpoint = (window.innerWidth <= 500) ? 1 : 2;
+    this.width = (window.innerWidth <= 500) ? 280 : 500;
+    this.height = (window.innerWidth <= 500) ? 350 : 642;
   }
 
+  @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.breakpoint = (event.target.innerWidth <= 500) ? 1 : 3;
-    this.rowpoint = (window.innerWidth <= 500) ? 1 : 2;
+    this.width = (event.target.innerWidth <= 1400) ? (this.width - (this.width * 0.02)) : 500;
+    this.height = (event.target.innerWidth <= 1400) ? (this.height - (this.height * 0.02)) : 642;
+    if (event.target.innerWidth > this.prevInner){
+      this.width = (this.width + (this.width * 0.04));
+      this.height = (this.height + (this.height * 0.04));
+    }
+    this.prevInner = event.target.innerWidth;
+    if (this.width < 280){
+      this.width = 280;
+    }
+    if (this.height < 350){
+      this.height = 350;
+    }
+    if(this.width > 500){
+      this.width = 500;
+    }
+    if(this.height > 642){
+      this.height = 642;
+    }
   }
 
   ngOnDestroy(): void {
